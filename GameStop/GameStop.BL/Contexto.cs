bdeea0cb2a7e51834d7cc9e6b437deba2e20c0ca
juego <juego@DESKTOP-SN82M72>
2022-07-06 21:12:59 -0600

    Agregando controlador, clase BL para las ordenes

--- GameStop/GameStop.BL/Contexto.cs
+++ GameStop/GameStop.BL/Contexto.cs
@@ -19,8 +19,13 @@ namespace GameStop.BL
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            
         }
         public DbSet<Producto> Productos { get; set; }
         public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+
+        public DbSet<Orden> Ordenes { get; set; }
+        public DbSet<OrdenDetalle> OrdenDetalle { get; set; }
     }
 }
